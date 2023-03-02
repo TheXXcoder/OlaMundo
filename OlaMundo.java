16770a1f857784ebac7fc95835d4d5385259771e
CarolVianaXX <124326973+TheXXcoder@users.noreply.github.com>
2023-03-02 11:14:35 -0300

    Add files via upload

---
+++ OlaMundo.java
@@ -0,0 +1,8 @@
+
+public class OlaMundo {      //declaração da classe
+ 
+    public static void main(String args[]){ // Bloco principal, que inicia a aplicação
+     
+        System.out.println("Ola Mundo"); // diretiva que imprime no console
+    } 
+}
